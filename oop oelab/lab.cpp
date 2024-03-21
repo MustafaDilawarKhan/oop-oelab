909ee3fc3a9e44a4af7d98bf8e12dcf6e911d6bb
MustafaDilawarKhan <khanmustafadilawar@gmail.com>
2024-03-21 12:02:02 +0500

    Student class created

--- oop oelab/lab.cpp
+++ oop oelab/lab.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+class Student {
+public:
+    string studentID;
+    string name;
+    string email;
+};
+
+
+
+
+int main() {
+
+
+};
